6f31a68f6bcbc7127e2f429a2b8e5fd21f78e2d2
HappyPresto <darkstararekku@gmail.com>
2020-09-15 16:53:31 +0300

    Add files via upload
    some work with hooks

---
+++ src/components/SongList.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1},
+        { title: 'memory gospel', id: 2}, 
+        { title: 'this wild darkness', id: 3}
+    ]);
+    const addSong = () => {
+        setSongs([
+            ...songs, {title: 'new song', id: songs.length + 1}
+        ])
+    }
+    return ( 
+        <div className="song-list">
+            <ul>
+                {songs.map(song => {
+                    return (
+                        <li key={song.id}>{song.title}</li>
+                    )
+                })}
+            </ul>
+            <button onClick={addSong}>Add Song</button>
+        </div>
+    );
+}
+ 
+export default SongList;
